ac1154f4f7e91cf09d7abe0934876f1e656d96e3
pora <porapat.ongkan@gmail.com>
2020-11-27 23:40:55 +0900

    ADD: time related utility functions

---
+++ utils.py
@@ -0,0 +1,21 @@
+import datetime
+
+DATETIME_STRING_FORMAT = "%Y-%m-%dT%H:%M:%S"
+
+
+def dt2ts(dt):
+    if type(dt) == int:
+        return dt
+    elif type(dt) == str:
+        dt = str2dt(dt)
+    return int((dt - datetime.datetime(1970, 1, 1)).total_seconds())
+
+
+def str2dt(s, fmt="%Y-%m-%d %H:%M:%S %Z"):
+    if type(s) == int:
+        return ts2dt(s)
+    return datetime.datetime.strptime(s, fmt)
+
+
+def ts2dt(ts):
+    return datetime.datetime.fromtimestamp(ts + datetime.datetime(1970, 1, 1).timestamp())
