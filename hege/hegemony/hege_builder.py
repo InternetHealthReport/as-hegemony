e102c6cd471eacbb823a7afbe1923e650cc7a38d
pora <porapat.ongkan@gmail.com>
2020-12-16 20:36:06 +0900

    EDIT: change hegemony builder output format

--- hege/hegemony/hege_builder.py
+++ hege/hegemony/hege_builder.py
@@ -1,81 +1,46 @@
-from collections import defaultdict
 import json
-import logging
 
-from hege.bcscore.bcscore_loader import BCSCORELoader
-from hege.utils.utils import str_datetime_to_timestamp
+from hege.hegemony.hege_builder_helper import HegeBuilderHelper
+from hege.utils import utils
+
+with open("/app/config.json", "r") as f:
+    config = json.load(f)
+HEGE_DATA_TOPIC = config["hege"]["data_topic"]
+HEGE_META_DATA_TOPIC = config["hege"]["meta_data_topic"]
+INTERVAL = config["hege"]["dump_interval"]
 
 
 class HegeBuilder:
-    def __init__(self, collectors: list, timestamp: int):
+    def __init__(self, collectors, start_timestamp: int, end_timestamp: int):
         self.collectors = collectors
-        self.timestamp = timestamp
-        self.hegemony_score_list = defaultdict(lambda: defaultdict(list))
-        self.hegemony_score = defaultdict(dict)
+        self.start_timestamp = start_timestamp
+        self.end_timestamp = end_timestamp
 
-    def build_hegemony_score(self):
-        for collector in self.collectors:
-            logging.debug(f"read {collector}'s bcscore")
-            self.read_data_for_as_hegemony(collector)
-        self.calculate_hegemony()
+        self.kafka_data_topic = HEGE_DATA_TOPIC
+        self.kafka_meta_data_topic = HEGE_META_DATA_TOPIC
 
-    def read_data_for_as_hegemony(self, collector: str):
-        loaded_bcscore = BCSCORELoader(collector, self.timestamp).load_data()
-        logging.debug(f"read collector {collector}'s bcscore; {len(loaded_bcscore)} ases data loaded")
+    def consume_and_calculate(self):
+        for timestamp in range(self.start_timestamp, self.end_timestamp, INTERVAL):
+            hege_builder_helper = HegeBuilderHelper(self.collectors, timestamp)
+            hege_builder_helper.build_hegemony_score()
+            yield timestamp, self.dump_as_hegemony_score(hege_builder_helper)
 
-        for scope_asn in loaded_bcscore:
-            self.load_scoped_average_bcscore_list(loaded_bcscore[scope_asn], scope_asn)
-
-    def load_scoped_average_bcscore_list(self, depended_asn_bcscore: dict, scope_asn: str):
-        for depended_asn in depended_asn_bcscore:
-
-            sum_bcscore_score_in_asn = defaultdict(int)
-            peers_count_in_asn = defaultdict(int)
-            for peer_asn, peer_bcscore in depended_asn_bcscore[depended_asn]:
-                sum_bcscore_score_in_asn[peer_asn] += peer_bcscore
-                peers_count_in_asn[peer_asn] += 1
-
-            for peer_asn in sum_bcscore_score_in_asn:
-                as_bcscore = sum_bcscore_score_in_asn[peer_asn] / peers_count_in_asn[peer_asn]
-                if as_bcscore != 0:
-                    self.hegemony_score_list[scope_asn][depended_asn].append(as_bcscore)
-
-    def calculate_hegemony(self):
-        logging.info("start calculating as hegemony score")
-        for i, scope_asn in enumerate(self.hegemony_score_list):
-            self.calculate_hegemony_helper(scope_asn)
-
-    def calculate_hegemony_helper(self, scope_asn: str):
-        scope_asn_hegemony_score_list = self.hegemony_score_list[scope_asn]
-        total_asn_count = len(scope_asn_hegemony_score_list[scope_asn])
-
-        ten_percent = int(total_asn_count*0.1)
-        ninety_percent = int(total_asn_count*0.9)
-        _range = ninety_percent - ten_percent
-        if _range == 0:
-            return
-
-        for asn in scope_asn_hegemony_score_list:
-            asn_count = len(scope_asn_hegemony_score_list[asn])
-            peers_bc_score_list = [0] * (total_asn_count - asn_count) + scope_asn_hegemony_score_list[asn]
-            sorted_peers_bc_score_list = sorted(peers_bc_score_list)
-            hege_score = sum(sorted_peers_bc_score_list[ten_percent:ninety_percent]) / _range
-            if hege_score != 0:
-                self.hegemony_score[scope_asn][asn] = hege_score
+    @staticmethod
+    def dump_as_hegemony_score(hege_builder_helper):
+        hegemony_scope = hege_builder_helper.hegemony_score
+        for scope in hegemony_scope:
+            yield hegemony_scope[scope], scope
 
 
 if __name__ == "__main__":
-    FORMAT = '%(asctime)s %(name)s %(message)s'
-    logging.basicConfig(
-        format=FORMAT, level=logging.DEBUG, datefmt='%Y-%m-%d %H:%M:%S'
-    )
-
-    bcscore_time_string = "2020-08-01T00:00:00"
-    bcscore_timestamp = str_datetime_to_timestamp(bcscore_time_string)
+    start_time_string = "2020-08-01T00:00:00"
+    start_ts = utils.str_datetime_to_timestamp(start_time_string)
 
     test_collectors = ["rrc00", "rrc10", "route-views.linx", "route-views2"]
-    hege_builder = HegeBuilder(test_collectors, bcscore_timestamp)
-    hege_builder.build_hegemony_score()
+    hege_builder = HegeBuilder(test_collectors, start_ts, start_ts + 901)
 
-    with open("/app/test-hegemony-builder-result.json", "w") as f:
-        json.dump(hege_builder.hegemony_score, f, indent=4)
+    for ts, asn_hege_generator in hege_builder.consume_and_calculate():
+        for message, scope_asn in asn_hege_generator:
+            print(scope_asn, message)
+            break
+        break
---
+++ hege/hegemony/hege_builder_helper.py
@@ -0,0 +1,81 @@
+from collections import defaultdict
+import json
+import logging
+
+from hege.bcscore.bcscore_loader import BCSCORELoader
+from hege.utils.utils import str_datetime_to_timestamp
+
+
+class HegeBuilderHelper:
+    def __init__(self, collectors: list, timestamp: int):
+        self.collectors = collectors
+        self.timestamp = timestamp
+        self.hegemony_score_list = defaultdict(lambda: defaultdict(list))
+        self.hegemony_score = defaultdict(dict)
+
+    def build_hegemony_score(self):
+        for collector in self.collectors:
+            logging.debug(f"read {collector}'s bcscore")
+            self.read_data_for_as_hegemony(collector)
+        self.calculate_hegemony()
+
+    def read_data_for_as_hegemony(self, collector: str):
+        loaded_bcscore = BCSCORELoader(collector, self.timestamp).load_data()
+        logging.debug(f"read collector {collector}'s bcscore; {len(loaded_bcscore)} ases data loaded")
+
+        for scope_asn in loaded_bcscore:
+            self.load_scoped_average_bcscore_list(loaded_bcscore[scope_asn], scope_asn)
+
+    def load_scoped_average_bcscore_list(self, depended_asn_bcscore: dict, scope_asn: str):
+        for depended_asn in depended_asn_bcscore:
+
+            sum_bcscore_score_in_asn = defaultdict(int)
+            peers_count_in_asn = defaultdict(int)
+            for peer_asn, peer_bcscore in depended_asn_bcscore[depended_asn]:
+                sum_bcscore_score_in_asn[peer_asn] += peer_bcscore
+                peers_count_in_asn[peer_asn] += 1
+
+            for peer_asn in sum_bcscore_score_in_asn:
+                as_bcscore = sum_bcscore_score_in_asn[peer_asn] / peers_count_in_asn[peer_asn]
+                if as_bcscore != 0:
+                    self.hegemony_score_list[scope_asn][depended_asn].append(as_bcscore)
+
+    def calculate_hegemony(self):
+        logging.info("start calculating as hegemony score")
+        for i, scope_asn in enumerate(self.hegemony_score_list):
+            self.calculate_hegemony_helper(scope_asn)
+
+    def calculate_hegemony_helper(self, scope_asn: str):
+        scope_asn_hegemony_score_list = self.hegemony_score_list[scope_asn]
+        total_asn_count = len(scope_asn_hegemony_score_list[scope_asn])
+
+        ten_percent = int(total_asn_count*0.1)
+        ninety_percent = int(total_asn_count*0.9)
+        _range = ninety_percent - ten_percent
+        if _range == 0:
+            return
+
+        for asn in scope_asn_hegemony_score_list:
+            asn_count = len(scope_asn_hegemony_score_list[asn])
+            peers_bc_score_list = [0] * (total_asn_count - asn_count) + scope_asn_hegemony_score_list[asn]
+            sorted_peers_bc_score_list = sorted(peers_bc_score_list)
+            hege_score = sum(sorted_peers_bc_score_list[ten_percent:ninety_percent]) / _range
+            if hege_score != 0:
+                self.hegemony_score[scope_asn][asn] = hege_score
+
+
+if __name__ == "__main__":
+    FORMAT = '%(asctime)s %(name)s %(message)s'
+    logging.basicConfig(
+        format=FORMAT, level=logging.DEBUG, datefmt='%Y-%m-%d %H:%M:%S'
+    )
+
+    bcscore_time_string = "2020-08-01T00:00:00"
+    bcscore_timestamp = str_datetime_to_timestamp(bcscore_time_string)
+
+    test_collectors = ["rrc00", "rrc10", "route-views.linx", "route-views2"]
+    hege_builder = HegeBuilderHelper(test_collectors, bcscore_timestamp)
+    hege_builder.build_hegemony_score()
+
+    with open("/app/test-hegemony-builder-result.json", "w") as f:
+        json.dump(hege_builder.hegemony_score, f, indent=4)
