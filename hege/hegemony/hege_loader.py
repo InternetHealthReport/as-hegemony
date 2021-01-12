ecf718bf8006c4af8a217295b9ad6d27e7709d14
pora <porapat.ongkan@gmail.com>
2021-01-12 14:53:24 +0900

    ADD: script for querying hegemony score

--- hege/hegemony/hege_loader.py
+++ hege/hegemony/hege_loader.py
@@ -6,49 +6,44 @@ from hege.utils import kafka_data, utils
 
 with open("/app/config.json", "r") as f:
     config = json.load(f)
-ASHEGE_TOPIC = config["hege"]["data_topic"]
+AS_HEGE_TOPIC = config["hege"]["data_topic__as"]
+PREFIX_HEGE_TOPIC = config["hege"]["data_topic__prefix"]
 
 
-def __get_hege_consumer(start_timestamp: int):
-    return kafka_data.create_consumer_and_set_offset(ASHEGE_TOPIC, start_timestamp)
+def __get_hege_consumer(start_timestamp: int, is_asn_query):
+    if is_asn_query:
+        return kafka_data.create_consumer_and_set_offset(AS_HEGE_TOPIC, start_timestamp)
+    else:
+        return kafka_data.create_consumer_and_set_offset(PREFIX_HEGE_TOPIC, start_timestamp)
 
 
-def get_hegemony_score_for_asn_at(asn: str, timestamp: int):
-    consumer = __get_hege_consumer(timestamp)
+def get_hegemony_score_for_scope_at(selected_scope: str, timestamp: int):
+    is_asn_query = "as" in selected_scope
+    consumer = __get_hege_consumer(timestamp, is_asn_query)
+
     for message, _ in kafka_data.consume_stream(consumer):
         message_timestamp = message["timestamp"]
-        scope_asn = message["scope"]
+        message_scope = message["scope"]
         if message_timestamp != timestamp:
             return
-        if asn == scope_asn:
+        if message_scope == selected_scope:
+            print(f"found hegemony score for {selected_scope}")
             return message["scope_hegemony"]
-
-
-def get_hegemony_score_at(timestamp: int):
-    hege_score = dict()
-    consumer = __get_hege_consumer(timestamp)
-    for message, _ in kafka_data.consume_stream(consumer):
-        message_timestamp = message["timestamp"]
-        scope_asn = message["scope"]
-        if message_timestamp != timestamp:
-            break
-        hege_score[scope_asn] = message["scope_hegemony"]
-    return hege_score
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--selected_time", "-t", help="choose the as hegemony time")
-    parser.add_argument("--asn", "-a", help="choose asn")
+    parser.add_argument("--scope", "-s", help="choose scope")
     # Example: 2020-08-01T00:00:00
 
     args = parser.parse_args()
     assert args.selected_time
 
     selected_timestamp = utils.str_datetime_to_timestamp(args.selected_time)
-    scope_asn = args.asn
+    scope = args.scope
 
-    if scope_asn:
-        print(get_hegemony_score_for_asn_at(scope_asn, selected_timestamp))
+    if scope:
+        print(get_hegemony_score_for_scope_at(scope, selected_timestamp))
     else:
-        print(get_hegemony_score_at(selected_timestamp))
+        print("please select scope")
