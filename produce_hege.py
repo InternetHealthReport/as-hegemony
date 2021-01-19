3fe1ee18c9854a11b238bf88962b8e72552154de
pora <porapat.ongkan@gmail.com>
2021-01-19 11:36:57 +0900

    EDIT: move collectors params to args

--- produce_hege.py
+++ produce_hege.py
@@ -11,18 +11,20 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description=text)
     parser.add_argument("--start_time", "-s", help="Choose the start time")
-    parser.add_argument("--end_time", "-e", help="Choose the end time ")
-    parser.add_argument(
-        "--prefix", "-p",
-        help="if you used this flag, script will run in prefix hege mode",
-        action='store_true'
-    )
+    parser.add_argument("--end_time", "-e", help="Choose the end time")
+    parser.add_argument("--collectors", "-c",
+                        help="Choose your collectors: it should be written in the following patterns"
+                             "collector1,collector2,collector3")
+    parser.add_argument("--prefix", "-p",
+                        help="if you used this flag, script will run in prefix hege mode",
+                        action='store_true')
+
     # Example: 2020-08-01T00:00:00
 
     args = parser.parse_args()
     assert args.start_time and args.end_time
 
-    selected_collectors = ["rrc00", "rrc10", "route-views.linx", "route-views2"]
+    selected_collectors = list(map(lambda x: x.strip(), args.collectors.split(",")))
     start_time_string = args.start_time
     end_time_string = args.end_time
 
